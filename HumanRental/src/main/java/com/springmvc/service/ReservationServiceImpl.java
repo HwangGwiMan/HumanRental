4e7722e190a6048e7f00b250ac7a1583f4d1d509
jeonghwan <enfg4859@gmail.com>
2024-02-23 17:00:48 +0900

    네비 간격 수정, 쿼리 변경 및 변경에 의한 예약 코드 수정

--- HumanRental/src/main/java/com/springmvc/service/ReservationServiceImpl.java
+++ HumanRental/src/main/java/com/springmvc/service/ReservationServiceImpl.java
@@ -15,8 +15,13 @@ import org.springframework.ui.Model;
 import com.springmvc.domain.Board;
 import com.springmvc.domain.Reservation;
 import com.springmvc.domain.Buying;
+import com.springmvc.domain.Mentee;
+import com.springmvc.domain.Mentor;
 import com.springmvc.repository.ReservationRepository;
 import com.springmvc.repository.BuyingRepository;
+import com.springmvc.repository.MemberRepository;
+import com.springmvc.repository.MenteeRepository;
+import com.springmvc.repository.MentorRepository;
 import com.springmvc.util.Utility;
 
 @Repository
@@ -28,26 +33,40 @@ public class ReservationServiceImpl implements ReservationService{
 	@Autowired
 	BuyingRepository buyingrepository;
 	
+	@Autowired
+	MenteeRepository menteerepository;
+	
+	@Autowired
+	MentorRepository mentorrepository;
+	
+	@Autowired
+	MemberRepository memberrepository;
+	
 	Utility util = new Utility();
 
 	@Override
-	public Reservation ReservationCreate(String buyingId, String date, String content, String memberId) {
-		
-		// 추후에 멘티, 멘토 아이디로 변경해야함
+	public Reservation ReservationCreate(String buyingId, String date, String content, String memberId, Model model) {
 		
 		Buying buying = buyingrepository.BuyingDetailbyId(buyingId);
+		Mentee mentee=menteerepository.getInformation(buying.getMemberId());
+		Mentor mentor=mentorrepository.getMentor(memberId);
 		
 		Reservation reservation = new Reservation();
 		reservation.setReservationId(util.createId("Reservation"));
 		reservation.setType("buy");
 		reservation.setTitle(buying.getTitle());
-		reservation.setMenteeId(buying.getMemberId());
-		reservation.setMentorId(memberId);
+		reservation.setMenteeId(mentee.getMenteeId());
+		reservation.setMentorId(mentor.getMentorId());
 		reservation.setReservationdate(LocalDate.parse(date));
 		reservation.setReservationcontent(content);
 		reservation.setApprove(false);
 		
 		reservationrepository.ReservationCreate(reservation);
+		
+		String menteeNickname = memberrepository.getMember(mentee.getMemberId()).getNickName();
+		String mentorNickname = memberrepository.getMember(mentor.getMemberId()).getNickName();
+		model.addAttribute("menteeNickname",menteeNickname);
+		model.addAttribute("mentorNickname",mentorNickname);
 		
 		return reservation;
 	}
